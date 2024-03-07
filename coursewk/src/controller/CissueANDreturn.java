9ccc8dd31e9c3552822e21c88d9940963248dd75
sanjana <sanjanaabeysiri1234567@gmail.com>
2024-03-08 00:20:54 +0530

    END

--- coursewk/src/controller/CissueANDreturn.java
+++ coursewk/src/controller/CissueANDreturn.java
@@ -14,50 +14,51 @@ import model.*;
 public class CissueANDreturn {
     ResultSet rs4;
     ResultSet rs5;
-    public void addIssuedetails(int issueid,String issuememberid,String issuemembername,String issuememberbookisbn,String issuememberbookname,String issuedate,String issuereturndate)
-    {
+
+    public void addIssuedetails(int issueid, String issuememberid, String issuemembername, String issuememberbookisbn,
+            String issuememberbookname, String issuedate, String issuereturndate) {
         MissueANDreturn mir = new MissueANDreturn();
-        mir.Maddissue(issueid,issuememberid,issuemembername,issuememberbookisbn,issuememberbookname,issuedate,issuereturndate); 
+        mir.Maddissue(issueid, issuememberid, issuemembername, issuememberbookisbn, issuememberbookname, issuedate,
+                issuereturndate);
     }
-   
-    public void updateIssuedetails(int issueid,String issuememberid,String issuemembername,String issuememberbookisbn,String issuememberbookname,String issuedate,String issuereturndate)
-    {
+
+    public void updateIssuedetails(int issueid, String issuememberid, String issuemembername,
+            String issuememberbookisbn, String issuememberbookname, String issuedate, String issuereturndate) {
         MissueANDreturn mir = new MissueANDreturn();
-        mir.Mupdateissue(issueid,issuememberid,issuemembername,issuememberbookisbn,issuememberbookname,issuedate,issuereturndate); 
- 
+        mir.Mupdateissue(issueid, issuememberid, issuemembername, issuememberbookisbn, issuememberbookname, issuedate,
+                issuereturndate);
+
     }
-    
-     public void deleteIssuedetails(int deleteissueid)
-    {
+
+    public void deleteIssuedetails(int deleteissueid) {
         MissueANDreturn mir = new MissueANDreturn();
-        mir.issuedeletedDataDetails(deleteissueid); 
-   
+        mir.issuedeletedDataDetails(deleteissueid);
+
     }
-     
-     public ResultSet IssueViewDetails()
-    {
+
+    public ResultSet IssueViewDetails() {
         MissueANDreturn mir = new MissueANDreturn();
-        rs4 = mir.IssueViewDataDetails(); 
+        rs4 = mir.IssueViewDataDetails();
         return rs4;
-    } 
-     
-      public void addreturndetails(int returnid,int issueid2,String todaydate,int dayselap,String fine,String status)
-    {
-        MissueANDreturn mir = new MissueANDreturn();
-        mir.Maddreturn(returnid,issueid2,todaydate,dayselap,fine,status); 
     }
-      
-       public ResultSet returnViewDetails()
-    {
+
+    public void addreturndetails(int returnid, int issueid2, String todaydate, int dayselap, String fine,
+            String status) {
         MissueANDreturn mir = new MissueANDreturn();
-        rs5 = mir.returnViewDataDetails(); 
+        mir.Maddreturn(returnid, issueid2, todaydate, dayselap, fine, status);
+    }
+
+    public ResultSet returnViewDetails() {
+        MissueANDreturn mir = new MissueANDreturn();
+        rs5 = mir.returnViewDataDetails();
         return rs5;
-    } 
-       
-       public void deletereturndetails(int returndeleteid)
-    {
+    }
+
+    public void deletereturndetails(int returndeleteid) {
         MissueANDreturn mir = new MissueANDreturn();
-        mir.returndeletedDataDetails(returndeleteid); 
-   
-    } 
+        mir.returndeletedDataDetails(returndeleteid);
+
+    }
 }
+
+// End
